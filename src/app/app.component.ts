9c1d03a39be34d2de200ee3c60fbd360119c3813
josue baez <josuebaez61@gmail.com>
2024-09-09 22:19:18 -0300

    Se agrego interpolacion de variable nombre y showAlert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'clase-01-introduccion';
+  nombre = 'Josue';
+
+  showAlert() {
+    alert('Hello!');
+  }
 }
